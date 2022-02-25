dd5d1aef4281c45211c57c318a56809fcfd43de0
Yu Liin Wu <yulin.wu@mail.utoronto.ca>
2022-02-25 14:12:43 -0500

    2022-02-25 2PM Yu Lin More Codes

--- Python.py
+++ Python.py
@@ -1,4 +1,4 @@
-#Import packages
+# ----------------Import packages
 import os
 import json
 import pandas as pd
@@ -7,20 +7,49 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pylab as plt
 
-#Panda Dataframe Set Up
-#SBA_Loan = pd.read_csv('SBAnational.csv',low_memory = False)
+# ----------------Panda Dataframe Set Up
+SBA_Loan = pd.read_csv('SBAnational.csv', low_memory=False)
+SBA_Loan = SBA_Loan.sort_values(by='ApprovalFY', ascending=True)
+SBA_Loan['ApprovalDate'] = pd.to_datetime(SBA_Loan['ApprovalDate'])
+SBA_Loan_TX = SBA_Loan[SBA_Loan['State'] == 'TX']
+
+
 US_President = pd.read_csv('US_President.csv')
 
-#print(SBA_Loan.info())  #check column names
-#print(US_President.info())  #check column names
+# print(SBA_Loan.info())  #check column names
+# print(US_President.info())  #check column names
 
-#print(SBA_Loan['ApprovalFY'].values.min())  #check the earliest date, output = 1962
-#print(SBA_Loan['ApprovalFY'].values.max())  #check the latest date, output = 2014
+# print(SBA_Loan['ApprovalFY'].values.min())  #check the earliest date, output = 1962
+# print(SBA_Loan['ApprovalFY'].values.max())  #check the latest date, output = 2014
 
 US_President['start date'] = pd.to_datetime(US_President['From'])
 
-US_President['end date'] = pd.to_datetime(US_President['To'],errors = 'coerce') #the last end date is current, which is a string, replace it with NAT
-US_President['end date'] = US_President['end date'].fillna(pd.to_datetime('2021-01-20')) #Replace NAT with the googled last day of Trump's presidency term
+US_President['end date'] = pd.to_datetime(US_President['To'],errors='coerce')  # the last end date is current, which is a string, replace it with NAT
+US_President['end date'] = US_President['end date'].fillna(
+pd.to_datetime('2021-01-20'))  # Replace NAT with the googled last day of Trump's presidency term
+
+#----------------Need to drop the dollar under some columns in SBA_Loan and convert the type to float
 
 
-print(US_President['end date'])
+# --------------Create a function for merging based timestamp between two dates, run time is long, optimization could occur
+def merge_on_date(df_A, df_B, df_A_column, df_B_column):
+    for index, row in df_B.iterrows():
+        start_date = row['start date']
+        end_date = row['end date']
+        for index1, row1 in df_A.iterrows():
+            if row1['ApprovalDate'] < end_date and row1['ApprovalDate'] >= start_date:
+                df_A.at[index1,df_A_column] = row[df_B_column]
+
+    return df_A
+
+#---------------Testing the function above
+#SBA_Loan_TX = merge_on_date(SBA_Loan_TX,US_President,'President','President')
+SBA_Loan_TX = merge_on_date(SBA_Loan_TX,US_President,'President Party','Party')
+#print(SBA_Loan_TX.head())
+#print(SBA_Loan_TX.info())
+
+#------------------Visualization
+SBA_Loan_TX_grouped = SBA_Loan_TX.groupby('President Party')['DisbursementGross'].sum()
+print(SBA_Loan_TX_grouped)
+
+
